4dbccb91148681eb97c1b5cf8a084afd780a053b
Philipp Weidel <p.weidel@fz-juelich.de>
2015-02-17 21:01:27 +0100

    added background push service

--- Kicker/app/src/main/java/de/fz_juelich/inm/kicker/kicker/Kicker.java
+++ Kicker/app/src/main/java/de/fz_juelich/inm/kicker/kicker/Kicker.java
@@ -1,8 +1,12 @@
 package de.fz_juelich.inm.kicker.kicker;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by weidel on 15.02.15.
@@ -12,5 +16,16 @@ public class Kicker extends Application {
     @Override
     public void onCreate(){
         Parse.initialize(this, "8VWYCZEvlRgpevlb1xwgFsxrzGG6zOPnnfz9hGLV", "RQ9tdA7ZrQjXhYi5e0pLue7Pkr1j8EkR003YR1rE");
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
+
     }
 }
